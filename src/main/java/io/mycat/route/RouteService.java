2e8ba6fbea42c9e685a1ec6d0f58e81ef0585e1e
yuwei <yuwei@yuweix.com>
2018-02-26 16:22:27 +0800

    test

--- src/main/java/io/mycat/route/RouteService.java
+++ src/main/java/io/mycat/route/RouteService.java
@@ -23,16 +23,6 @@
  */
 package io.mycat.route;
 
-import java.sql.SQLNonTransientException;
-import java.sql.SQLSyntaxErrorException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.mycat.cache.CachePool;
 import io.mycat.cache.CacheService;
 import io.mycat.cache.LayerCachePool;
@@ -45,10 +35,18 @@ import io.mycat.route.handler.HintHandlerFactory;
 import io.mycat.route.handler.HintSQLHandler;
 import io.mycat.server.ServerConnection;
 import io.mycat.server.parser.ServerParse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLNonTransientException;
+import java.sql.SQLSyntaxErrorException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 public class RouteService {
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(RouteService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RouteService.class);
     public static final String MYCAT_HINT_TYPE = "_mycatHintType";
     private final CachePool sqlRouteCache;
 	private final LayerCachePool tableId2DataNodeCache;	
@@ -59,8 +57,7 @@ public class RouteService {
 
 	public RouteService(CacheService cachService) {
 		sqlRouteCache = cachService.getCachePool("SQLRouteCache");
-		tableId2DataNodeCache = (LayerCachePool) cachService
-				.getCachePool("TableID2DataNodeCache");
+		tableId2DataNodeCache = (LayerCachePool) cachService.getCachePool("TableID2DataNodeCache");
 	}
 
 	public LayerCachePool getTableId2DataNodeCache() {
@@ -80,7 +77,7 @@ public class RouteService {
 			cacheKey = schema.getName() + stmt;			
 			rrs = (RouteResultset) sqlRouteCache.get(cacheKey);
 			if (rrs != null) {
-				checkMigrateRule(schema.getName(),rrs,sqlType);
+				checkMigrateRule(schema.getName(), rrs, sqlType);
 				return rrs;
 			}
 		}
@@ -99,88 +96,87 @@ public class RouteService {
 				String hint = stmt.substring(hintLength, endPos).trim();	
 				
                 int firstSplitPos = hint.indexOf(HINT_SPLIT);                
-                if(firstSplitPos > 0 ){
-                    Map hintMap=    parseHint(hint);
+                if(firstSplitPos > 0){
+                    Map hintMap = parseHint(hint);
                 	String hintType = (String) hintMap.get(MYCAT_HINT_TYPE);
                     String hintSql = (String) hintMap.get(hintType);
-                    if( hintSql.length() == 0 ) {
+                    if(hintSql.length() == 0) {
                     	LOGGER.warn("comment int sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: "+stmt);
                     	throw new SQLSyntaxErrorException("comment int sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: "+stmt);
                     }
                     String realSQL = stmt.substring(endPos + "*/".length()).trim();
 
                     HintHandler hintHandler = HintHandlerFactory.getHintHandler(hintType);
-                    if( hintHandler != null ) {    
-
-                    	if ( hintHandler instanceof  HintSQLHandler) {                    		
+                    if(hintHandler != null) {
+                    	if (hintHandler instanceof HintSQLHandler) {
                           	/**
                         	 * 修复 注解SQL的 sqlType 与 实际SQL的 sqlType 不一致问题， 如： hint=SELECT，real=INSERT
                         	 * fixed by zhuam
                         	 */
                     		int hintSqlType = ServerParse.parse( hintSql ) & 0xff;     
-                    		rrs = hintHandler.route(sysconf, schema, sqlType, realSQL, charset, sc, tableId2DataNodeCache, hintSql,hintSqlType,hintMap);
+                    		rrs = hintHandler.route(sysconf, schema, sqlType, realSQL, charset, sc, tableId2DataNodeCache, hintSql, hintSqlType, hintMap);
                     		
                     	} else {                    		
-                    		rrs = hintHandler.route(sysconf, schema, sqlType, realSQL, charset, sc, tableId2DataNodeCache, hintSql,sqlType,hintMap);
+                    		rrs = hintHandler.route(sysconf, schema, sqlType, realSQL, charset, sc, tableId2DataNodeCache, hintSql, sqlType, hintMap);
                     	}
  
-                    }else{
+                    } else {
                         LOGGER.warn("TODO , support hint sql type : " + hintType);
                     }
                     
-                }else{//fixed by runfriends@126.com
-                	LOGGER.warn("comment in sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: "+stmt);
-                	throw new SQLSyntaxErrorException("comment in sql must meet :/*!mcat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: "+stmt);
+                } else {//fixed by runfriends@126.com
+                	LOGGER.warn("comment in sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: " + stmt);
+                	throw new SQLSyntaxErrorException("comment in sql must meet :/*!mcat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: " + stmt);
                 }
 			}
 		} else {
 			stmt = stmt.trim();
-			rrs = RouteStrategyFactory.getRouteStrategy().route(sysconf, schema, sqlType, stmt,
-					charset, sc, tableId2DataNodeCache);
+			rrs = RouteStrategyFactory.getRouteStrategy().route(sysconf, schema, sqlType, stmt, charset, sc, tableId2DataNodeCache);
 		}
 
 		if (rrs != null && sqlType == ServerParse.SELECT && rrs.isCacheAble()) {
 			sqlRouteCache.putIfAbsent(cacheKey, rrs);
 		}
-		checkMigrateRule(schema.getName(),rrs,sqlType);
+		checkMigrateRule(schema.getName(), rrs, sqlType);
 		return rrs;
 	}
 
 	 //数据迁移的切换准备阶段，需要拒绝写操作和所有的跨多节点写操作
-		private   void checkMigrateRule(String schemal,RouteResultset rrs,int sqlType ) throws SQLNonTransientException {
-		if(rrs!=null&&rrs.getTables()!=null){
-			boolean isUpdate=isUpdateSql(sqlType);
-			if(!isUpdate)return;
-			ConcurrentMap<String,List<PartitionByCRC32PreSlot.Range>> tableRules= RouteCheckRule.migrateRuleMap.get(schemal.toUpperCase()) ;
-			if(tableRules!=null){
-			for (String table : rrs.getTables()) {
-				List<PartitionByCRC32PreSlot.Range> rangeList= tableRules.get(table.toUpperCase()) ;
-				if(rangeList!=null&&!rangeList.isEmpty()){
-					if(rrs.getNodes().length>1&&isUpdate){
-						throw new   SQLNonTransientException ("schema:"+schemal+",table:"+table+",sql:"+rrs.getStatement()+" is not allowed,because table is migrate switching,please wait for a moment");
-					}
-					for (PartitionByCRC32PreSlot.Range range : rangeList) {
-						RouteResultsetNode[] routeResultsetNodes=	rrs.getNodes();
-						for (RouteResultsetNode routeResultsetNode : routeResultsetNodes) {
-							int slot=routeResultsetNode.getSlot();
-							if(isUpdate&&slot>=range.start&&slot<=range.end){
-								throw new   SQLNonTransientException ("schema:"+schemal+",table:"+table+",sql:"+rrs.getStatement()+" is not allowed,because table is migrate switching,please wait for a moment");
-
+	private void checkMigrateRule(String schemal, RouteResultset rrs, int sqlType ) throws SQLNonTransientException {
+		if(rrs != null && rrs.getTables() != null) {
+			boolean isUpdate = isUpdateSql(sqlType);
+			if(!isUpdate) {
+				return;
+			}
+			ConcurrentMap<String, List<PartitionByCRC32PreSlot.Range>> tableRules = RouteCheckRule.migrateRuleMap.get(schemal.toUpperCase()) ;
+			if(tableRules != null) {
+				for (String table : rrs.getTables()) {
+					List<PartitionByCRC32PreSlot.Range> rangeList = tableRules.get(table.toUpperCase()) ;
+					if(rangeList != null && !rangeList.isEmpty()) {
+						if(rrs.getNodes().length > 1 && isUpdate){
+							throw new SQLNonTransientException ("schema:" + schemal + ", table:" + table + ", sql:" + rrs.getStatement() + " is not allowed, because table is migrate switching, please wait for a moment.");
+						}
+						for (PartitionByCRC32PreSlot.Range range : rangeList) {
+							RouteResultsetNode[] routeResultsetNodes = rrs.getNodes();
+							for (RouteResultsetNode routeResultsetNode : routeResultsetNodes) {
+								int slot = routeResultsetNode.getSlot();
+								if(isUpdate && slot >= range.start && slot <= range.end) {
+									throw new SQLNonTransientException ("schema:" + schemal + ", table:" + table + ", sql:" + rrs.getStatement() + " is not allowed, because table is migrate switching, please wait for a moment.");
+								}
 							}
 						}
 					}
 				}
-			}
 			}
 		}
 	}
 
 
 	private boolean isUpdateSql(int type) {
-		return ServerParse.INSERT==type||ServerParse.UPDATE==type||ServerParse.DELETE==type||ServerParse.DDL==type;
+		return ServerParse.INSERT == type || ServerParse.UPDATE == type || ServerParse.DELETE == type || ServerParse.DDL == type;
 	}
 
-	public static int isHintSql(String sql){
+	public static int isHintSql(String sql) {
 		int j = 0;
 		int len = sql.length();
 		if(sql.charAt(j++) == '/' && sql.charAt(j++) == '*'){
@@ -193,7 +189,7 @@ public class RouteService {
 			//注解支持的'#'不被mybatis兼容
 			//注解支持的':'不被hibernate兼容
 			//考虑用mycat字符前缀标志Hintsql:"/** mycat: */"
-			if(sql.charAt(j)=='m'){
+			if(sql.charAt(j) == 'm'){
 				j--;
 			}
 			if(j + 6 >= len)	{// prevent the following sql.charAt overflow
@@ -207,54 +203,38 @@ public class RouteService {
 		return -1;	// false
 	}
 	
-	 private   Map parseHint( String sql)
-    {
-        Map map=new HashMap();
-        int y=0;
-        int begin=0;
-        for(int i=0;i<sql.length();i++)
-        {
-            char cur=sql.charAt(i);
-            if(cur==','&& y%2==0)
-            {
+	private Map parseHint(String sql){
+        Map map = new HashMap();
+        int y = 0;
+        int begin = 0;
+        for(int i = 0; i < sql.length(); i++) {
+            char cur = sql.charAt(i);
+            if(cur == ',' && y % 2 == 0) {
                 String substring = sql.substring(begin, i);
-
                 parseKeyValue(map, substring);
-                begin=i+1;
-            }
-            else
-            if(cur=='\'')
-            {
+                begin = i + 1;
+            } else if(cur == '\'') {
                 y++;
-            } if(i==sql.length()-1)
-        {
-            parseKeyValue(map, sql.substring(begin));
-
-        }
-
-
+            }
+            if(i == sql.length() - 1) {
+            	parseKeyValue(map, sql.substring(begin));
+        	}
         }
         return map;
     }
 
-    private  void parseKeyValue(Map map, String substring)
-    {
+    private void parseKeyValue(Map map, String substring) {
         int indexOf = substring.indexOf('=');
-        if(indexOf!=-1)
-        {
-
-            String key=substring.substring(0,indexOf).trim().toLowerCase();
-            String value=substring.substring(indexOf+1,substring.length());
-            if(value.endsWith("'")&&value.startsWith("'"))
-            {
-                value=value.substring(1,value.length()-1);
+        if(indexOf != -1) {
+            String key = substring.substring(0, indexOf).trim().toLowerCase();
+            String value = substring.substring(indexOf + 1, substring.length());
+            if(value.endsWith("'") && value.startsWith("'")) {
+                value = value.substring(1, value.length() - 1);
             }
-            if(map.isEmpty())
-            {
-              map.put(MYCAT_HINT_TYPE,key)  ;
+            if(map.isEmpty()) {
+ 				map.put(MYCAT_HINT_TYPE,key);
             }
-            map.put(key,value.trim());
-
+            map.put(key, value.trim());
         }
     }
 }
