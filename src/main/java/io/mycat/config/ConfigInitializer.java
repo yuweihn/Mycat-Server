f2cc8f9fc3c1ef4c83a4b872e089871c0f36a5fe
yuwei <yuwei@yuweix.com>
2018-04-10 17:40:30 +0800

    test

--- src/main/java/io/mycat/config/ConfigInitializer.java
+++ src/main/java/io/mycat/config/ConfigInitializer.java
@@ -23,14 +23,6 @@
  */
 package io.mycat.config;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import io.mycat.config.loader.zkprocess.comm.ZkConfig;
-import io.mycat.config.loader.zkprocess.comm.ZkParamCfg;
-import org.apache.log4j.Logger;
 
 import io.mycat.backend.datasource.PhysicalDBNode;
 import io.mycat.backend.datasource.PhysicalDBPool;
@@ -42,24 +34,26 @@ import io.mycat.config.loader.ConfigLoader;
 import io.mycat.config.loader.SchemaLoader;
 import io.mycat.config.loader.xml.XMLConfigLoader;
 import io.mycat.config.loader.xml.XMLSchemaLoader;
-import io.mycat.config.model.DBHostConfig;
-import io.mycat.config.model.DataHostConfig;
-import io.mycat.config.model.DataNodeConfig;
-import io.mycat.config.model.FirewallConfig;
-import io.mycat.config.model.SchemaConfig;
-import io.mycat.config.model.SystemConfig;
-import io.mycat.config.model.UserConfig;
+import io.mycat.config.loader.zkprocess.comm.ZkConfig;
+import io.mycat.config.loader.zkprocess.comm.ZkParamCfg;
+import io.mycat.config.model.*;
 import io.mycat.config.util.ConfigException;
 import io.mycat.route.sequence.handler.DistributedSequenceHandler;
 import io.mycat.route.sequence.handler.IncrSequenceMySQLHandler;
 import io.mycat.route.sequence.handler.IncrSequenceTimeHandler;
 import io.mycat.route.sequence.handler.IncrSequenceZKHandler;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 
 /**
  * @author mycat
  */
 public class ConfigInitializer {
-	
 	private static final Logger LOGGER = Logger.getLogger( ConfigInitializer.class );
 	
 	private volatile SystemConfig system;
@@ -71,13 +65,11 @@ public class ConfigInitializer {
 	private volatile Map<String, PhysicalDBPool> dataHosts;
 
 	public ConfigInitializer(boolean loadDataHost) {
-		
 		//读取rule.xml和schema.xml
 		SchemaLoader schemaLoader = new XMLSchemaLoader();
 		
 		//读取server.xml
 		XMLConfigLoader configLoader = new XMLConfigLoader(schemaLoader);
-		
 		schemaLoader = null;
 		
 		//加载配置
