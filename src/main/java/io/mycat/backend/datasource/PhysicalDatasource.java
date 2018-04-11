96f66c9ff3d16b86c0fe520df15a591a0ef3a7b4
yuwei <yuwei@yuweix.com>
2018-04-11 11:29:11 +0800

    test

--- src/main/java/io/mycat/backend/datasource/PhysicalDatasource.java
+++ src/main/java/io/mycat/backend/datasource/PhysicalDatasource.java
@@ -438,7 +438,8 @@ public abstract class PhysicalDatasource {
 		return conn;
 	}
 
-	private void createNewConnection(final ResponseHandler handler, final Object attachment, final String schema) {
+	private void createNewConnection(final ResponseHandler handler, final Object attachment
+			, final String schema) {
 		// aysn create connection
 		MycatServer.getInstance().getBusinessExecutor().execute(new Runnable() {
 			public void run() {
