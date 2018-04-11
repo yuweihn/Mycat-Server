afe63584782ba8d74bf66db8a832d4949b32705a
yuwei <yuwei@yuweix.com>
2018-04-11 17:56:34 +0800

    test

--- src/main/java/io/mycat/route/sequence/handler/IncrSequenceHandler.java
+++ src/main/java/io/mycat/route/sequence/handler/IncrSequenceHandler.java
@@ -23,10 +23,12 @@
  */
 package io.mycat.route.sequence.handler;
 
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
 
 /**
  * 递增序列号处理器
@@ -39,17 +41,16 @@ public abstract class IncrSequenceHandler implements SequenceHandler {
 	public static final Logger logger = LoggerFactory.getLogger(IncrSequenceHandler.class);
 
 	public static final String FILE_NAME = "sequence_conf.properties";
-
 	public static final String KEY_HIS_NAME = ".HISIDS";// 1-10000,50001-60000
 	public static final String KEY_MIN_NAME = ".MINID";// 1
 	public static final String KEY_MAX_NAME = ".MAXID";// 10000
 	public static final String KEY_CUR_NAME = ".CURID";// 888
 
+
 	public abstract Map<String, String> getParaValMap(String prefixName);
-
 	public abstract Boolean updateCURIDVal(String prefixName, Long val);
-
 	public abstract Boolean fetchNextPeriod(String prefixName);
+
 
 	@Override
 	public long nextId(String prefixName) {
@@ -65,6 +66,5 @@ public abstract class IncrSequenceHandler implements SequenceHandler {
 		}
 		updateCURIDVal(prefixName, nextId);
 		return nextId.longValue();
-
 	}
 }
