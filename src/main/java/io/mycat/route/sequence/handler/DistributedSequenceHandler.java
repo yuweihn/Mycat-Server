9c730e5de2f3b1d58829fb23b1a34ae8810cb9e5
yuwei <yuwei@yuweix.com>
2018-04-11 17:42:22 +0800

    test

--- src/main/java/io/mycat/route/sequence/handler/DistributedSequenceHandler.java
+++ src/main/java/io/mycat/route/sequence/handler/DistributedSequenceHandler.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Executors;
