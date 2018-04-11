1745b3f3a5d0f5ae9be38d191263982f45b2900a
yuwei <yuwei@yuweix.com>
2018-04-11 10:47:08 +0800

    test

--- src/main/java/io/mycat/config/ConfigInitializer.java
+++ src/main/java/io/mycat/config/ConfigInitializer.java
@@ -111,14 +111,12 @@ public class ConfigInitializer {
 	}
 	
 	private void selfChecking0() throws ConfigException {
-		
 		// 检查user与schema配置对应以及schema配置不为空
 		if (users == null || users.isEmpty()) {
 			throw new ConfigException("SelfCheck### user all node is empty!");
 			
 		} else {
-			
-			for (UserConfig uc : users.values()) {
+			for (UserConfig uc: users.values()) {
 				if (uc == null) {
 					throw new ConfigException("SelfCheck### users node within the item is empty!");
 				}
@@ -128,63 +126,55 @@ public class ConfigInitializer {
 					throw new ConfigException("SelfCheck### user " + uc.getName() + "refered schemas is empty!");
 				}
 				
-				for (String schema : authSchemas) {
-					if ( !schemas.containsKey(schema) ) {
+				for (String schema: authSchemas) {
+					if (!schemas.containsKey(schema)) {
 						String errMsg = "SelfCheck###  schema " + schema + " refered by user " + uc.getName() + " is not exist!";
 						throw new ConfigException(errMsg);
 					}
 				}
 			}
 		}	
-		
-		
+
 		// schema 配置检测		
 		for (SchemaConfig sc : schemas.values()) {
 			if (null == sc) {
 				throw new ConfigException("SelfCheck### schema all node is empty!");
-				
-			} else {				
+			} else {
 				// check dataNode / dataHost 节点
-				if ( this.dataNodes != null &&  this.dataHosts != null  ) {					
+				if (this.dataNodes != null && this.dataHosts != null) {
 					Set<String> dataNodeNames = sc.getAllDataNodes();
 					for(String dataNodeName: dataNodeNames) {
-						
 						PhysicalDBNode node = this.dataNodes.get(dataNodeName);
-						if ( node == null ) {
+						if (node == null) {
 							throw new ConfigException("SelfCheck### schema dbnode is empty!");
 						}
 					}
 				}
 			}
-		}	
-		
+		}
 	}
 	
 	public void testConnection() {
-		
 		// 实际链路的连接测试		
-		if ( this.dataNodes != null &&  this.dataHosts != null  ) {
-			
+		if (this.dataNodes != null && this.dataHosts != null) {
 			Map<String, Boolean> map = new HashMap<String, Boolean>();
-			
-			for(PhysicalDBNode dataNode: dataNodes.values() ) {
-				
+			for(PhysicalDBNode dataNode: dataNodes.values()) {
 				String database = dataNode.getDatabase();		
 				PhysicalDBPool pool = dataNode.getDbPool();
 				
 				for (PhysicalDatasource ds : pool.getAllDataSources()) {							
 					String key = ds.getName() + "_" + database;
-					if ( map.get( key ) == null ) {										
-						map.put( key, false );
+					if (map.get(key) == null) {
+						map.put(key, false);
 						
 						boolean isConnected = false;
 						try {
-							isConnected = ds.testConnection( database );		
-							map.put( key, isConnected );
+							isConnected = ds.testConnection(database);
+							map.put(key, isConnected);
 						} catch (IOException e) {
 							LOGGER.warn("test conn error:", e);
-						}										
-					}								
+						}
+					}
 				}
 			}
 			
@@ -193,21 +183,18 @@ public class ConfigInitializer {
 			for (Map.Entry<String, Boolean> entry : map.entrySet()) {
 				String key = entry.getKey();
 				Boolean value = entry.getValue();
-				if ( !value && isConnectivity ) {
-					LOGGER.warn("SelfCheck### test " + key + " database connection failed ");							
+				if (!value && isConnectivity) {
+					LOGGER.warn("SelfCheck### test " + key + " database connection failed ");
 					isConnectivity = false;
-					
 				} else {
 					LOGGER.info("SelfCheck### test " + key + " database connection success ");
 				}
 			}
 			
-			if ( !isConnectivity ) {
+			if (!isConnectivity) {
 				throw new ConfigException("SelfCheck### there are some datasource connection failed, pls check!");
 			}
-				
 		}
-		
 	}
 
 	public SystemConfig getSystem() {
@@ -244,12 +231,11 @@ public class ConfigInitializer {
 
 	private Map<String, PhysicalDBPool> initDataHosts(ConfigLoader configLoader) {
 		Map<String, DataHostConfig> nodeConfs = configLoader.getDataHosts();
-		boolean isBooster="booster".equalsIgnoreCase(ZkConfig.getInstance().getValue(ZkParamCfg.MYCAT_SERVER_TYPE) ) ;
+		boolean isBooster = "booster".equalsIgnoreCase(ZkConfig.getInstance().getValue(ZkParamCfg.MYCAT_SERVER_TYPE));
 		//根据DataHost建立PhysicalDBPool，其实就是实际数据库连接池，每个DataHost对应一个PhysicalDBPool
-		Map<String, PhysicalDBPool> nodes = new HashMap<String, PhysicalDBPool>(
-				nodeConfs.size());
-		for (DataHostConfig conf : nodeConfs.values()) {
-			if(isBooster){
+		Map<String, PhysicalDBPool> nodes = new HashMap<String, PhysicalDBPool>(nodeConfs.size());
+		for (DataHostConfig conf: nodeConfs.values()) {
+			if(isBooster) {
 				conf.setMinCon(2);
 			}
 			//建立PhysicalDBPool
@@ -259,9 +245,8 @@ public class ConfigInitializer {
 		return nodes;
 	}
 
-	private PhysicalDatasource[] createDataSource(DataHostConfig conf,
-			String hostName, String dbType, String dbDriver,
-			DBHostConfig[] nodes, boolean isRead) {
+	private PhysicalDatasource[] createDataSource(DataHostConfig conf, String hostName, String dbType
+			, String dbDriver, DBHostConfig[] nodes, boolean isRead) {
 		PhysicalDatasource[] dataSources = new PhysicalDatasource[nodes.length];
 		if (dbType.equals("mysql") && dbDriver.equals("native")) {
 			for (int i = 0; i < nodes.length; i++) {
@@ -270,67 +255,56 @@ public class ConfigInitializer {
 				MySQLDataSource ds = new MySQLDataSource(nodes[i], conf, isRead);
 				dataSources[i] = ds;
 			}
-
 		} else if (dbDriver.equals("jdbc")) {
 			for (int i = 0; i < nodes.length; i++) {
 				nodes[i].setIdleTimeout(system.getIdleTimeout());
 				JDBCDatasource ds = new JDBCDatasource(nodes[i], conf, isRead);
 				dataSources[i] = ds;
 			}
-		} else if ("postgresql".equalsIgnoreCase(dbType) && dbDriver.equalsIgnoreCase("native")){
+		} else if ("postgresql".equalsIgnoreCase(dbType) && dbDriver.equalsIgnoreCase("native")) {
 			for (int i = 0; i < nodes.length; i++) {
 				nodes[i].setIdleTimeout(system.getIdleTimeout());
 				PostgreSQLDataSource ds = new PostgreSQLDataSource(nodes[i], conf, isRead);
 				dataSources[i] = ds;
 			}
-		} else{
+		} else {
 			throw new ConfigException("not supported yet !" + hostName);
 		}
 		return dataSources;
 	}
 
-	private PhysicalDBPool getPhysicalDBPool(DataHostConfig conf,
-			ConfigLoader configLoader) {
+	private PhysicalDBPool getPhysicalDBPool(DataHostConfig conf, ConfigLoader configLoader) {
 		String name = conf.getName();
 		//数据库类型，我们这里只讨论MySQL
 		String dbType = conf.getDbType();
 		//连接数据库驱动，我们这里只讨论MyCat自己实现的native
 		String dbDriver = conf.getDbDriver();
 		//针对所有写节点创建PhysicalDatasource
-		PhysicalDatasource[] writeSources = createDataSource(conf, name,
-				dbType, dbDriver, conf.getWriteHosts(), false);
+		PhysicalDatasource[] writeSources = createDataSource(conf, name, dbType, dbDriver, conf.getWriteHosts(), false);
 		Map<Integer, DBHostConfig[]> readHostsMap = conf.getReadHosts();
-		Map<Integer, PhysicalDatasource[]> readSourcesMap = new HashMap<Integer, PhysicalDatasource[]>(
-				readHostsMap.size());
+		Map<Integer, PhysicalDatasource[]> readSourcesMap = new HashMap<Integer, PhysicalDatasource[]>(readHostsMap.size());
 		//对于每个读节点建立key为writeHost下标value为readHost的PhysicalDatasource[]的哈希表
 		for (Map.Entry<Integer, DBHostConfig[]> entry : readHostsMap.entrySet()) {
-			PhysicalDatasource[] readSources = createDataSource(conf, name,
-					dbType, dbDriver, entry.getValue(), true);
+			PhysicalDatasource[] readSources = createDataSource(conf, name, dbType, dbDriver, entry.getValue(), true);
 			readSourcesMap.put(entry.getKey(), readSources);
 		}
-		PhysicalDBPool pool = new PhysicalDBPool(conf.getName(), conf,
-				writeSources, readSourcesMap, conf.getBalance(),
-				conf.getWriteType());
+		PhysicalDBPool pool = new PhysicalDBPool(conf.getName(), conf, writeSources, readSourcesMap, conf.getBalance(), conf.getWriteType());
 		pool.setSlaveIDs(conf.getSlaveIDs());
 		return pool;
 	}
 
 	private Map<String, PhysicalDBNode> initDataNodes(ConfigLoader configLoader) {
 		Map<String, DataNodeConfig> nodeConfs = configLoader.getDataNodes();
-		Map<String, PhysicalDBNode> nodes = new HashMap<String, PhysicalDBNode>(
-				nodeConfs.size());
-		for (DataNodeConfig conf : nodeConfs.values()) {
+		Map<String, PhysicalDBNode> nodes = new HashMap<String, PhysicalDBNode>(nodeConfs.size());
+		for (DataNodeConfig conf: nodeConfs.values()) {
 			PhysicalDBPool pool = this.dataHosts.get(conf.getDataHost());
 			if (pool == null) {
-				throw new ConfigException("dataHost not exists "
-						+ conf.getDataHost());
-
+				throw new ConfigException("dataHost not exists " + conf.getDataHost());
 			}
-			PhysicalDBNode dataNode = new PhysicalDBNode(conf.getName(),
-					conf.getDatabase(), pool);
+			PhysicalDBNode dataNode = new PhysicalDBNode(conf.getName(), conf.getDatabase(), pool);
 			nodes.put(dataNode.getName(), dataNode);
 		}
 		return nodes;
 	}
-
 }
+
