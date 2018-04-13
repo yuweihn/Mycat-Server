c07b8d6f05efc8c65e34fe98036c3cb3e16b4209
yuwei <yu.wei@niuhome.com>
2018-04-13 21:29:30 +0800

    test

--- src/main/java/io/mycat/config/Versions.java
+++ src/main/java/io/mycat/config/Versions.java
@@ -23,31 +23,30 @@
  */
 package io.mycat.config;
 
-
-
 /**
  * @author mycat
  */
 public abstract class Versions {
+
     /**协议版本**/
     public static final byte PROTOCOL_VERSION = 10;
-    /**服务器版本**/
-    public static byte[] SERVER_VERSION = "5.6.29-mycat-1.6.5-release-20171115110454".getBytes();
 
+    /**服务器版本**/
+    public static byte[] SERVER_VERSION = "5.6.29-mycat-1.6.5-release-20180413212112".getBytes();
 
     public static void setServerVersion(String version) {
         byte[] mysqlVersionPart = version.getBytes();
         int startIndex;
         for (startIndex = 0; startIndex < SERVER_VERSION.length; startIndex++) {
-            if (SERVER_VERSION[startIndex] == '-') {
+            if (SERVER_VERSION[startIndex] == '-')
                 break;
-            }
         }
 
         // 重新拼接mycat version字节数组
         byte[] newMycatVersion = new byte[mysqlVersionPart.length + SERVER_VERSION.length - startIndex];
         System.arraycopy(mysqlVersionPart, 0, newMycatVersion, 0, mysqlVersionPart.length);
-        System.arraycopy(SERVER_VERSION, startIndex, newMycatVersion, mysqlVersionPart.length, SERVER_VERSION.length - startIndex);
+        System.arraycopy(SERVER_VERSION, startIndex, newMycatVersion, mysqlVersionPart.length,
+                SERVER_VERSION.length - startIndex);
         SERVER_VERSION = newMycatVersion;
     }
 }
