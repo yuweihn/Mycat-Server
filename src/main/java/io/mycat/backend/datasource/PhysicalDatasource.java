944f6666ffaad8a37d5c7adc78903e93fa78b568
yuwei <yuwei@yuweix.com>
2018-04-13 16:25:39 +0800

    test

--- src/main/java/io/mycat/backend/datasource/PhysicalDatasource.java
+++ src/main/java/io/mycat/backend/datasource/PhysicalDatasource.java
@@ -549,7 +549,7 @@ public abstract class PhysicalDatasource {
 //		}
 		
 		if(!ok) {
-			LOGGER.warn("can't return to pool ,so close con " + c);
+			LOGGER.warn("can't return to pool, so close con " + c);
 			c.close("can't return to pool ");
 		}
 	}
