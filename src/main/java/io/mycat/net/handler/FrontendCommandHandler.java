dd07b35ad904c9b9b0675019b217fb0b8b573a83
yuwei <yuwei@yuweix.com>
2018-04-24 16:51:02 +0800

    test

--- src/main/java/io/mycat/net/handler/FrontendCommandHandler.java
+++ src/main/java/io/mycat/net/handler/FrontendCommandHandler.java
@@ -23,6 +23,7 @@
  */
 package io.mycat.net.handler;
 
+
 import io.mycat.backend.mysql.MySQLMessage;
 import io.mycat.config.ErrorCode;
 import io.mycat.net.FrontendConnection;
@@ -30,38 +31,32 @@ import io.mycat.net.NIOHandler;
 import io.mycat.net.mysql.MySQLPacket;
 import io.mycat.statistic.CommandCount;
 
+
 /**
  * 前端命令处理器
  *
  * @author mycat
  */
-public class FrontendCommandHandler implements NIOHandler
-{
-
+public class FrontendCommandHandler implements NIOHandler {
     protected final FrontendConnection source;
     protected final CommandCount commands;
 
-    public FrontendCommandHandler(FrontendConnection source)
-    {
+    public FrontendCommandHandler(FrontendConnection source) {
         this.source = source;
         this.commands = source.getProcessor().getCommands();
     }
 
     @Override
-    public void handle(byte[] data)
-    {
-        if(source.getLoadDataInfileHandler()!=null&&source.getLoadDataInfileHandler().isStartLoadData())
-        {
+    public void handle(byte[] data) {
+        if(source.getLoadDataInfileHandler() != null && source.getLoadDataInfileHandler().isStartLoadData()) {
             MySQLMessage mm = new MySQLMessage(data);
             int  packetLength = mm.readUB3();
-            if(packetLength+4==data.length)
-            {
+            if(packetLength + 4 == data.length) {
                 source.loadDataInfileData(data);
             }
             return;
         }
-        switch (data[4])
-        {
+        switch (data[4]) {
             case MySQLPacket.COM_INIT_DB:
                 commands.doInitDB();
                 source.initDB(data);
@@ -107,11 +102,8 @@ public class FrontendCommandHandler implements NIOHandler
                 source.heartbeat(data);
                 break;
             default:
-                     commands.doOther();
-                     source.writeErrMessage(ErrorCode.ER_UNKNOWN_COM_ERROR,
-                             "Unknown command");
-
+                commands.doOther();
+                source.writeErrMessage(ErrorCode.ER_UNKNOWN_COM_ERROR, "Unknown command");
         }
     }
-
 }
