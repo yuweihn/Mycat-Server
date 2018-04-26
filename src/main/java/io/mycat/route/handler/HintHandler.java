6b642993d3e4c34e16bb08bc6cf95d0477e9e764
yuwei <yuwei@yuweix.com>
2018-04-26 11:32:21 +0800

    test

--- src/main/java/io/mycat/route/handler/HintHandler.java
+++ src/main/java/io/mycat/route/handler/HintHandler.java
@@ -1,7 +1,5 @@
 package io.mycat.route.handler;
 
-import java.sql.SQLNonTransientException;
-import java.util.Map;
 
 import io.mycat.cache.LayerCachePool;
 import io.mycat.config.model.SchemaConfig;
@@ -9,14 +7,15 @@ import io.mycat.config.model.SystemConfig;
 import io.mycat.route.RouteResultset;
 import io.mycat.server.ServerConnection;
 
+import java.sql.SQLNonTransientException;
+import java.util.Map;
+
+
 /**
  * 按照注释中包含指定类型的内容做路由解析
- * 
  */
 public interface HintHandler {
-
-	public RouteResultset route(SystemConfig sysConfig, SchemaConfig schema,
-                                int sqlType, String realSQL, String charset, ServerConnection sc,
-                                LayerCachePool cachePool, String hintSQLValue, int hintSqlType, Map hintMap)
-			throws SQLNonTransientException;
+	RouteResultset route(SystemConfig sysConfig, SchemaConfig schema, int sqlType, String realSQL
+			, String charset, ServerConnection sc, LayerCachePool cachePool, String hintSQLValue
+			, int hintSqlType, Map hintMap) throws SQLNonTransientException;
 }
