0dc57c6b2b8aaf555a91a414bd95bc12e1c0a90e
yuwei <yuwei@yuweix.com>
2018-04-26 14:31:39 +0800

    test

--- src/main/java/io/mycat/memory/unsafe/row/StructType.java
+++ src/main/java/io/mycat/memory/unsafe/row/StructType.java
@@ -1,23 +1,25 @@
 package io.mycat.memory.unsafe.row;
 
+
 import io.mycat.sqlengine.mpp.ColMeta;
 import io.mycat.sqlengine.mpp.OrderCol;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
 
+
 /**
  * Created by zagnix on 2016/6/6.
  */
 public class StructType {
-
     private final Map<String, ColMeta> columToIndx;
     private final int fieldCount;
 
-    private  OrderCol[] orderCols = null;
+    private OrderCol[] orderCols = null;
 
-    public StructType(@Nonnull Map<String,ColMeta> columToIndx,int fieldCount){
-        assert fieldCount >=0;
+
+    public StructType(@Nonnull Map<String, ColMeta> columToIndx, int fieldCount) {
+        assert fieldCount >= 0;
         this.columToIndx = columToIndx;
         this.fieldCount = fieldCount;
     }
