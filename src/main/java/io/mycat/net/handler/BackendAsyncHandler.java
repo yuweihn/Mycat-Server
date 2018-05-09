4214a8c0a204b3dca923775d8f7d79c0724a2bf7
yuwei <yuwei@yuweix.com>
2018-05-09 17:14:34 +0800

    test

--- src/main/java/io/mycat/net/handler/BackendAsyncHandler.java
+++ src/main/java/io/mycat/net/handler/BackendAsyncHandler.java
@@ -23,27 +23,20 @@
  */
 package io.mycat.net.handler;
 
-import java.util.concurrent.Executor;
 
 import io.mycat.net.NIOHandler;
+import java.util.concurrent.Executor;
+
 
 /**
  * @author mycat
  */
 public abstract class BackendAsyncHandler implements NIOHandler {
-
 	protected void offerData(byte[] data, Executor executor) {
 		handleData(data);
-
-		// if (dataQueue.offer(data)) {
-		// handleQueue(executor);
-		// } else {
-		// offerDataError();
-		// }
 	}
 
 	protected abstract void offerDataError();
 
 	protected abstract void handleData(byte[] data);
-
 }
