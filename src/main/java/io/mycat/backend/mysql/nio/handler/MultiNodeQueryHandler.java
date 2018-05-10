5c977282dff23a3b64dc6099cc2d0983c8058489
yuwei <yuwei@yuweix.com>
2018-05-10 13:51:39 +0800

    test

--- src/main/java/io/mycat/backend/mysql/nio/handler/MultiNodeQueryHandler.java
+++ src/main/java/io/mycat/backend/mysql/nio/handler/MultiNodeQueryHandler.java
@@ -517,7 +517,7 @@ public class MultiNodeQueryHandler extends MultiNodeHandler implements LoadDataR
 			if (rrs.getLimitSize() < 0) {
 				end = results.size();
 			}
-				
+
 //			// 对于不需要排序的语句,返回的数据只有rrs.getLimitSize()
 //			if (rrs.getOrderByCols() == null) {
 //				end = results.size();
