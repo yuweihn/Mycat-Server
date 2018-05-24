03a12f263dfc2a08ccce1c8c86a9ab3e584075ba
yuwei <yu.wei@niuhome.com>
2018-05-24 23:03:52 +0800

    test

--- src/main/java/io/mycat/route/Procedure.java
+++ src/main/java/io/mycat/route/Procedure.java
@@ -1,10 +1,12 @@
 package io.mycat.route;
 
+
 import com.google.common.base.*;
 
 import java.io.Serializable;
 import java.sql.Types;
 import java.util.*;
+
 
 /**
  * Created by magicdoom on 2016/3/24.
@@ -33,151 +35,125 @@ import java.util.*;
  ok
 
  */
-public class Procedure implements Serializable
-{
+public class Procedure implements Serializable {
     private String originSql;
     private String name;
     private String callSql;
     private String setSql ;
     private String selectSql;
-    private Set<String> selectColumns=new LinkedHashSet<>();
-    private Set<String> listFields=new LinkedHashSet<>();
-    private boolean isResultList=false;
+    private Set<String> selectColumns = new LinkedHashSet<>();
+    private Set<String> listFields = new LinkedHashSet<>();
+    private boolean isResultList = false;
+    private Map<String,ProcedureParameter> paramterMap = new LinkedHashMap<>();
 
-    public boolean isResultList()
-    {
+
+    public boolean isResultList() {
         return isResultList;
     }
-    public boolean isResultSimpleValue()
-    {
-        return selectSql!=null&&!isResultList;
+    public boolean isResultSimpleValue() {
+        return selectSql != null&&!isResultList;
     }
-    public boolean isResultNothing()
-    {
-        return selectSql==null&&!isResultList;
+    public boolean isResultNothing() {
+        return selectSql == null && !isResultList;
     }
-    public void setResultList(boolean resultList)
-    {
+    public void setResultList(boolean resultList) {
         isResultList = resultList;
     }
 
-    public String toPreCallSql(String dbType)
-    {
-        StringBuilder sb=new StringBuilder();
-        sb.append("{ call ")  ;
+    public String toPreCallSql(String dbType) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ call ");
         sb.append(this.getName()).append("(") ;
-        Collection<ProcedureParameter> paramters=    this.getParamterMap().values();
-        int j=0;
-        for (ProcedureParameter paramter : paramters)
-        {
-
-            String name="?";
-            String joinStr=  j==this.getParamterMap().size()-1?name:name+"," ;
+        Collection<ProcedureParameter> paramters = this.getParamterMap().values();
+        int j = 0;
+        for (ProcedureParameter paramter: paramters) {
+            String name = "?";
+            String joinStr = j == this.getParamterMap().size() - 1 ? name : name + ",";
             sb.append(joinStr);
             j++;
         }
-        sb.append(")}")  ;
+        sb.append(")}");
         return sb.toString();
     }
 
-    public String toChangeCallSql(String dbType)
-    {
-        StringBuilder sb=new StringBuilder();
-        sb.append("call ")  ;
-        sb.append(this.getName()).append("(") ;
-        Collection<ProcedureParameter> paramters=    this.getParamterMap().values();
-        int j=0;
-        for (ProcedureParameter paramter : paramters)
-        {
-            Object value=paramter.getValue()!=null&& Types.VARCHAR==paramter.getJdbcType() ?"'"+paramter.getValue()+"'":paramter.getValue();
-             String name=paramter.getValue()==null?paramter.getName():String.valueOf(value);
-            String joinStr=  j==this.getParamterMap().size()-1?name:name+"," ;
+    public String toChangeCallSql(String dbType) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("call ");
+        sb.append(this.getName()).append("(");
+        Collection<ProcedureParameter> paramters = this.getParamterMap().values();
+        int j = 0;
+        for (ProcedureParameter paramter: paramters) {
+            Object value = paramter.getValue() != null && Types.VARCHAR == paramter.getJdbcType() ? "'" + paramter.getValue() + "'" : paramter.getValue();
+            String name = paramter.getValue() == null ? paramter.getName() : String.valueOf(value);
+            String joinStr = j == this.getParamterMap().size() - 1 ? name : name + ",";
             sb.append(joinStr);
             j++;
         }
-        sb.append(")")  ;
-        if(isResultSimpleValue())
-        {
+        sb.append(")");
+        if (isResultSimpleValue()) {
             sb.append(";select ");
-          sb.append(  Joiner.on(",").join(selectColumns)  );
+            sb.append(Joiner.on(",").join(selectColumns));
         }
         return sb.toString();
     }
 
-    public Set<String> getListFields()
-    {
+    public Set<String> getListFields() {
         return listFields;
     }
 
-    public void setListFields(Set<String> listFields)
-    {
+    public void setListFields(Set<String> listFields) {
         this.listFields = listFields;
     }
 
-    public Set<String> getSelectColumns()
-    {
+    public Set<String> getSelectColumns() {
         return selectColumns;
     }
 
-    public String getSetSql()
-    {
+    public String getSetSql() {
         return setSql;
     }
 
-    public void setSetSql(String setSql)
-    {
+    public void setSetSql(String setSql) {
         this.setSql = setSql;
     }
 
-    public String getSelectSql()
-    {
+    public String getSelectSql() {
         return selectSql;
     }
 
-    public void setSelectSql(String selectSql)
-    {
+    public void setSelectSql(String selectSql) {
         this.selectSql = selectSql;
     }
 
-    private Map<String,ProcedureParameter> paramterMap=new LinkedHashMap<>();
-
-    public String getOriginSql()
-    {
+    public String getOriginSql() {
         return originSql;
     }
 
-    public void setOriginSql(String originSql)
-    {
+    public void setOriginSql(String originSql) {
         this.originSql = originSql;
     }
 
-    public Map<String, ProcedureParameter> getParamterMap()
-    {
+    public Map<String, ProcedureParameter> getParamterMap() {
         return paramterMap;
     }
 
-    public void setParamterMap(Map<String, ProcedureParameter> paramterMap)
-    {
+    public void setParamterMap(Map<String, ProcedureParameter> paramterMap) {
         this.paramterMap = paramterMap;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getCallSql()
-    {
+    public String getCallSql() {
         return callSql;
     }
 
-    public void setCallSql(String callSql)
-    {
+    public void setCallSql(String callSql) {
         this.callSql = callSql;
     }
 }
