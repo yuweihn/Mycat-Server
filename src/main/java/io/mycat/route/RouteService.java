1d511bf4b63dc803ec321373076ee57ea7844a76
yuwei <yuwei@yuweix.com>
2018-05-25 13:49:06 +0800

    test

--- src/main/java/io/mycat/route/RouteService.java
+++ src/main/java/io/mycat/route/RouteService.java
@@ -89,26 +89,26 @@ public class RouteService {
 //      boolean isMatchNewHint = stmt.startsWith(NEW_MYCAT_HINT);
 //		if (isMatchOldHint || isMatchNewHint ) {
 		int hintLength = RouteService.isHintSql(stmt);
-		if(hintLength != -1) {
+		if (hintLength != -1) {
 			int endPos = stmt.indexOf("*/");
 			if (endPos > 0) {				
-				// 用!mycat:内部的语句来做路由分析
+//				用!mycat:内部的语句来做路由分析
 //				int hintLength = isMatchOldHint ? OLD_MYCAT_HINT.length() : NEW_MYCAT_HINT.length();
-				String hint = stmt.substring(hintLength, endPos).trim();	
+				String hint = stmt.substring(hintLength, endPos).trim();
 				
                 int firstSplitPos = hint.indexOf(HINT_SPLIT);                
-                if(firstSplitPos > 0) {
+                if (firstSplitPos > 0) {
                     Map hintMap = parseHint(hint);
                 	String hintType = (String) hintMap.get(MYCAT_HINT_TYPE);
                     String hintSql = (String) hintMap.get(hintType);
-                    if(hintSql.length() == 0) {
+                    if (hintSql.length() == 0) {
                     	LOGGER.warn("comment int sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: " + stmt);
                     	throw new SQLSyntaxErrorException("comment int sql must meet :/*!mycat:type=value*/ or /*#mycat:type=value*/ or /*mycat:type=value*/: " + stmt);
                     }
                     String realSQL = stmt.substring(endPos + "*/".length()).trim();
 
                     HintHandler hintHandler = HintHandlerFactory.getHintHandler(hintType);
-                    if(hintHandler != null) {
+                    if (hintHandler != null) {
                     	if (hintHandler instanceof HintSQLHandler) {
                           	/**
                         	 * 修复 注解SQL的 sqlType 与 实际SQL的 sqlType 不一致问题， 如： hint=SELECT，real=INSERT
@@ -141,24 +141,24 @@ public class RouteService {
 
 	 //数据迁移的切换准备阶段，需要拒绝写操作和所有的跨多节点写操作
 	private void checkMigrateRule(String schemal, RouteResultset rrs, int sqlType) throws SQLNonTransientException {
-		if(rrs != null && rrs.getTables() != null) {
+		if (rrs != null && rrs.getTables() != null) {
 			boolean isUpdate = isUpdateSql(sqlType);
-			if(!isUpdate) {
+			if (!isUpdate) {
 				return;
 			}
 			ConcurrentMap<String, List<PartitionByCRC32PreSlot.Range>> tableRules = RouteCheckRule.migrateRuleMap.get(schemal.toUpperCase()) ;
-			if(tableRules != null) {
+			if (tableRules != null) {
 				for (String table: rrs.getTables()) {
-					List<PartitionByCRC32PreSlot.Range> rangeList = tableRules.get(table.toUpperCase()) ;
-					if(rangeList != null && !rangeList.isEmpty()) {
-						if(rrs.getNodes().length > 1 && isUpdate){
+					List<PartitionByCRC32PreSlot.Range> rangeList = tableRules.get(table.toUpperCase());
+					if (rangeList != null && !rangeList.isEmpty()) {
+						if (rrs.getNodes().length > 1 && isUpdate) {
 							throw new SQLNonTransientException ("schema:" + schemal + ", table:" + table + ", sql:" + rrs.getStatement() + " is not allowed, because table is migrate switching, please wait for a moment.");
 						}
 						for (PartitionByCRC32PreSlot.Range range: rangeList) {
 							RouteResultsetNode[] routeResultsetNodes = rrs.getNodes();
 							for (RouteResultsetNode routeResultsetNode: routeResultsetNodes) {
 								int slot = routeResultsetNode.getSlot();
-								if(isUpdate && slot >= range.start && slot <= range.end) {
+								if (isUpdate && slot >= range.start && slot <= range.end) {
 									throw new SQLNonTransientException ("schema:" + schemal + ", table:" + table + ", sql:" + rrs.getStatement() + " is not allowed, because table is migrate switching, please wait for a moment.");
 								}
 							}
@@ -177,23 +177,23 @@ public class RouteService {
 	public static int isHintSql(String sql) {
 		int j = 0;
 		int len = sql.length();
-		if(sql.charAt(j++) == '/' && sql.charAt(j++) == '*') {
+		if (sql.charAt(j++) == '/' && sql.charAt(j++) == '*') {
 			char c = sql.charAt(j);
 			// 过滤掉 空格 和 * 两种字符, 支持： "/** !mycat: */" 和 "/** #mycat: */" 形式的注解
-			while(j < len && c != '!' && c != '#' && (c == ' ' || c == '*')) {
+			while (j < len && c != '!' && c != '#' && (c == ' ' || c == '*')) {
 				c = sql.charAt(++j);
 			}
 			//注解支持的'!'不被mysql单库兼容，
 			//注解支持的'#'不被mybatis兼容
 			//注解支持的':'不被hibernate兼容
 			//考虑用mycat字符前缀标志Hintsql:"/** mycat: */"
-			if(sql.charAt(j) == 'm') {
+			if (sql.charAt(j) == 'm') {
 				j--;
 			}
-			if(j + 6 >= len) {// prevent the following sql.charAt overflow
+			if (j + 6 >= len) {// prevent the following sql.charAt overflow
 				return -1;        // false
 			}
-			if(sql.charAt(++j) == 'm' && sql.charAt(++j) == 'y' && sql.charAt(++j) == 'c'
+			if (sql.charAt(++j) == 'm' && sql.charAt(++j) == 'y' && sql.charAt(++j) == 'c'
 				&& sql.charAt(++j) == 'a' && sql.charAt(++j) == 't' && (sql.charAt(++j) == ':' || sql.charAt(j) == '#')) {
 				return j + 1;    // true，同时返回注解部分的长度
 			}
@@ -205,17 +205,17 @@ public class RouteService {
         Map map = new HashMap();
         int y = 0;
         int begin = 0;
-        for(int i = 0; i < sql.length(); i++) {
+        for (int i = 0; i < sql.length(); i++) {
             char cur = sql.charAt(i);
-            if(cur == ',' && y % 2 == 0) {
+            if (cur == ',' && y % 2 == 0) {
                 String substring = sql.substring(begin, i);
                 parseKeyValue(map, substring);
                 begin = i + 1;
             } else if(cur == '\'') {
                 y++;
             }
-            if(i == sql.length() - 1) {
-            	parseKeyValue(map, sql.substring(begin));
+            if (i == sql.length() - 1) {
+				parseKeyValue(map, sql.substring(begin));
         	}
         }
         return map;
@@ -223,13 +223,13 @@ public class RouteService {
 
     private void parseKeyValue(Map map, String substring) {
         int indexOf = substring.indexOf('=');
-        if(indexOf != -1) {
+        if (indexOf != -1) {
             String key = substring.substring(0, indexOf).trim().toLowerCase();
             String value = substring.substring(indexOf + 1, substring.length());
-            if(value.endsWith("'") && value.startsWith("'")) {
+            if (value.endsWith("'") && value.startsWith("'")) {
                 value = value.substring(1, value.length() - 1);
             }
-            if(map.isEmpty()) {
+            if (map.isEmpty()) {
  				map.put(MYCAT_HINT_TYPE, key);
             }
             map.put(key, value.trim());
